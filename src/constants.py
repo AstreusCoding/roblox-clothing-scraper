7259d893816483101acc315683122997fe67ff9c
AstreusCoding <pandaa.casper@gmail.com>
2024-10-26 00:10:58 +0200

    Refactor group_handler.py: Update group URL template

--- src/constants.py
+++ src/constants.py
@@ -14,7 +14,7 @@ IMAGE_LOCATION_JSON_END = '"'
 
 # Centralized ROUTES dictionary for URL templates
 ROUTES = {
-    "group": "https://www.roblox.com/groups/{group_id}",  # Group page URL
+    "group_info": "https://groups.roblox.com/v1/groups/{group_id}",
     "catalog": "https://www.roblox.com/catalog/{catalog_id}",  # Specific catalog item URL
     "catalog_search": "https://catalog.roblox.com/v1/search/items",  # General catalog search API
     "base_asset": (
